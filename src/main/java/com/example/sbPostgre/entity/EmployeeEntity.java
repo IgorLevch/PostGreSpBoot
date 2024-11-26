407abfad9bafc10d0b53f13c30dc533f580aa25a
IgorL <ilevchenko406@gmail.com>
2024-11-26 20:41:12 +0300

    The ready project
    works by:
    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})

--- src/main/java/com/example/sbPostgre/entity/EmployeeEntity.java
+++ src/main/java/com/example/sbPostgre/entity/EmployeeEntity.java
@@ -1,16 +1,26 @@
 package com.example.sbPostgre.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 @Data
 @Table(name = "employees")
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeEntity {
+public class EmployeeEntity implements Serializable {
+
+
+
+    private static final long serialVersionUID = 1L;    // работает и без Serializable
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
